b6899abf898a1450a8fac3d840992a253999862c
nam <namhoangpham96@gmail.com>
2022-01-14 00:46:26 -0500

    Display change for the multiple search

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ function search1(){
     deleteList();
     const day = document.getElementById("specific").value;
     document.getElementById("error2").style = "display: none";
+    document.getElementById("multiple").style = "display:none";
     show(day);
 }
 
@@ -130,6 +131,7 @@ async function getSpecificInfo(day) {
 
 function search2(){
     document.getElementById("specific_infos").style = "display: none";
+    document.getElementById("multiple").style = "display:block";
     document.getElementById("multiple_loader").style = "display:block";
     document.getElementById("error2").style = "display: none";
     deleteList();
